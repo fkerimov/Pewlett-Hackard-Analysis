8b2a04b7a77789aed1261b21bdd1b82131c4ebb9
Farhad Kerimov <farhad.kerimov@gmail.com>
2021-09-08 10:57:40 -0400

    update comments

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,4 +1,4 @@
--- Create the retirement_titles.csv Steps 1-7
+-- Create the retirement_titles.csv
 SELECT e.emp_no,
 	e.first_name,
 	e.last_name,
@@ -11,7 +11,7 @@ INNER JOIN titles AS t ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no;
 
--- Create unique_titles table and export as per steps 8-14
+-- Create unique_titles table
 SELECT DISTINCT ON (emp_no) emp_no,
 	first_name,
 	last_name,
@@ -20,7 +20,7 @@ INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no ASC, to_date DESC;
 
--- Retrieve the number of employees by their most recent job title who are approaching retirement as per steps 15-21
+-- Retrieve the number of employees by their most recent job title who are approaching retirement
 SELECT COUNT(title),
 	title
 INTO retiring_titles
@@ -28,7 +28,7 @@ FROM unique_titles
 GROUP BY title
 ORDER BY "count" DESC;
 
--- Deliverable 2: Create a table of current employees eligible for the mentorship program
+-- Create a table of current employees eligible for the mentorship program
 
 SELECT DISTINCT ON (e.emp_no)e.emp_no,
 	e.first_name,
